4501afa2a0ae5904c10fd762a8a909b3e20cfc24
saadWindows <saadiqbal1921@gmail.com>
2024-08-03 15:32:09 +0530

    Auto Committed

--- mainfile.py
+++ mainfile.py
@@ -356,20 +356,7 @@ elif selection == 'TEXT SUMMARIZER from doc':
 
 
 elif selection == 'Email':
-    st.subheader('Email Service by saad@bravo')
-    a,b = st.columns(2)
-    to = a.text_input("Enter Recipient's Address")
-    fr = b.text_input("From (optional)")
-    msg = st.text_area('Enter Message to Send')
-    if fr is None:
-        fr = "test.service@saad"
-    
-    sub = st.button('Send')
-    if sub:
-        if to == "" or msg == "":
-            warn = st.warning("Field required")
-            time.sleep(2)
-            warn.empty()
+    pass
 
 
 else:
--- test.py
+++ test.py
@@ -1,38 +1,19 @@
+
 import streamlit as st
-from PyPDF2 import PdfReader
-from transformers import pipeline
-import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import time
 
-summarizer = pipeline("summarization")
+st.header('Email Service by saad.BravoAPI')
+a,b = st.columns(2)
+to = a.text_input("Enter Recipient's Address")
+fr = b.text_input("From (optional)")
+msg = st.text_area('Enter Message to Send')
+if fr =="":
+    fr = "test.service@saad"
 
-def extract_text_from_pdf(pdf_file):
-    reader = PdfReader(pdf_file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
-
-def summarize_text(text):
-    max_chunk = 1000
-    text_chunks = [text[i:i+max_chunk] for i in range(0, len(text), max_chunk)]
-    summary = ""
-    for chunk in text_chunks:
-        summary_text = summarizer(chunk, max_length=150, min_length=50, do_sample=False)[0]['summary_text']
-        summary += summary_text + "\n"
-    return summary
-
-st.title("PDF Content Summarizer")
-
-uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
-if uploaded_file is not None:
-    with st.spinner("Extracting text from PDF..."):
-        text = extract_text_from_pdf(uploaded_file)
-    st.subheader("Extracted Text:")
-    st.write(text)
-
-    if st.button("Summarize"):
-        with st.spinner("Summarizing content..."):
-            summary = summarize_text(text)
-        st.subheader("Summary:")
-        st.write(summary)
+sub = st.button('Send')
+if sub:
+    if to == "" or msg == "":
+        st.subheader(fr)
+        warn = st.warning("Field required")
+        time.sleep(2)
+        warn.empty()
