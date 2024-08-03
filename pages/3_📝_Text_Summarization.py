6f090b0f1852c97d50db45c2335e28b91c22ac36
saadWindows <saadiqbal1921@gmail.com>
2024-08-04 01:35:56 +0530

    Auto Committed

--- pages/3_📝_Text_Summarization.py
+++ pages/3_📝_Text_Summarization.py
@@ -4,15 +4,14 @@ st.set_page_config(
     page_title="Text Summarizer",
     page_icon="📝",
 )
-msg = "COMING SOON!!"
 
-message_container = st.empty()
+msg = "Coming Soon"
 
-msg = "COMING SOON!!"
+def stream_data():
+    for word in msg.split(" "):
+        yield word + " "
+        time.sleep(0.02)
 
-# Display each word with a delay
-for word in msg.split(" "):
-    message_container.markdown(f"## {word}")
-    time.sleep(0.5)  # Adjust the delay as needed
-# st.header("Coming Soon")
-st.snow()
+
+
+st.write_stream(stream_data)
