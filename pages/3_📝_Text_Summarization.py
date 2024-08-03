f2e78be7b72cb1b95ec92465a329c1f573e8988b
saadWindows <saadiqbal1921@gmail.com>
2024-08-04 01:31:51 +0530

    Auto Committed

--- pages/3_📝_Text_Summarization.py
+++ pages/3_📝_Text_Summarization.py
@@ -4,6 +4,10 @@ st.set_page_config(
     page_title="Text Summarizer",
     page_icon="📝",
 )
+msg = "COMING SOON!!"
 
-st.header("Coming Soon")
+for word in msg.split(" "):
+        yield word + " "
+        time.sleep(0.2)
+# st.header("Coming Soon")
 st.snow()
