92dcbd06d9d575f1744ea9355510e38fed8772f9
saadWindows <saadiqbal1921@gmail.com>
2024-08-04 01:37:25 +0530

    Auto Committed

--- pages/3_📝_Text_Summarization.py
+++ pages/3_📝_Text_Summarization.py
@@ -1,18 +1,8 @@
 import streamlit as st
-import time
 
 st.set_page_config(
     page_title="Text Summarizer",
     page_icon="📝",
 )
 
-msg = """Coming Soon"""
-
-def stream_data():
-    for word in msg.split(" "):
-        yield word + " "
-        time.sleep(0.02)
-
-
-
-st.write_stream(stream_data)
+st.header("Coming Soon")
