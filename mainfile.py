9b42f3ce4269fa03562a23e0667334141b769686
saadWindows <saadiqbal1921@gmail.com>
2024-08-03 15:19:29 +0530

    Auto Committed

--- mainfile.py
+++ mainfile.py
@@ -365,7 +365,7 @@ elif selection == 'Email':
         fr = "test.service@saad"
     
     sub = st.button('Send')
-    if fr is None or msg is None:
+    if fr is None or msg is None and sub:
         warn = st.warning("Field required")
         time.sleep(2)
         warn.empty()
