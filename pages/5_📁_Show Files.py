6e3b1185e5d48593389c80665d3fbd2f9bb4cecd
saadWindows <saadiqbal1921@gmail.com>
2024-08-04 13:43:25 +0530

    Auto Committed

--- pages/5_📁_Show Files.py
+++ pages/5_📁_Show Files.py
@@ -5,4 +5,23 @@ st.set_page_config(
     page_icon="📁",
 )
 
-st.header("Coming Soon")
+st.header('🚀Github FILES')
+# passw = st.text_input('Enter Password', type="password")
+def read_file_content(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
+valid_extensions = ('txt','py','cpp','java','json','js','html','css','bat','c','kt')
+files = [''] + [file for file in os.listdir() if file.endswith(valid_extensions)]
+selected_file = st.selectbox('Select file', files)
+if selected_file:
+    if selected_file != '':
+        st.header(f"{selected_file}")
+        content = read_file_content(selected_file)
+        st.code(content, language='python')
+    else:
+        st.info("Select a file from the dropdown to view its content.")
+else:
+    st.text("Please select a file from the dropdown.")
+# elif passw != st.secrets["psd"] and passw != '' :
+#     st.error('wrong person !!')
