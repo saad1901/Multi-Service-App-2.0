bf9f6b4d05895f11ca7f8bbde80988e48de2910f
saadWindows <saadiqbal1921@gmail.com>
2024-08-05 03:01:18 +0530

    Auto Committed

--- pages/15_🗺️_Map.py
+++ pages/15_🗺️_Map.py
@@ -3,7 +3,6 @@ import streamlit as st
 from streamlit_folium import st_folium
 from geopy.geocoders import Nominatim
 
-# Function to get coordinates for a given place name
 def get_coordinates(place_name):
     geolocator = Nominatim(user_agent="streamlit_app")
     location = geolocator.geocode(place_name)
@@ -12,12 +11,8 @@ def get_coordinates(place_name):
     else:
         return None, None
 
-# Create a Streamlit app
-st.title("Place Search and Map Marker")
-
-# Input for the place name
+st.header("Maps")
 place_name = st.text_input("Enter a place name or address:")
-
 
 map_center = [19.873383111857923, 75.3285979664069]
 m = folium.Map(location=map_center, zoom_start=16)
@@ -38,5 +33,4 @@ if place_name:
     else:
         st.error("Could not find the place. Please try another place name or address.")
 
-# Render the map in Streamlit
-st_data = st_folium(m, width=725, height=500)
+st_data = st_folium(m, width=625, height=400)
