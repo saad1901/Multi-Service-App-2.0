428f9a1d6d4d8e4a3286e0921c6d9053ba92d476
saadWindows <saadiqbal1921@gmail.com>
2024-08-05 16:25:10 +0530

    Auto Committed

--- pages/14_♻️_File_Type_Converter.py
+++ pages/14_♻️_File_Type_Converter.py
@@ -1,11 +1,11 @@
 import streamlit as st
 st.logo('images/banner-app-Photoroom.png', icon_image='images/image.png')
-t1,t2,t3 = st.tabs(["Docx to PDF","JPG to PDF","PPT to PDF"])
 
 st.set_page_config(
     page_title="File Converter",
     page_icon="♻️",
 )
+t1,t2,t3 = st.tabs(["Docx to PDF","JPG to PDF","PPT to PDF"])
 
 with t1:
     st.header('Coming very Soon')
